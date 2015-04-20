12a38d2a52e0155e04518fe8cbbd8e9ce7d71a49
Yhon Danilo Tabares Ramirez <yhondanilo@gmail.com>
2015-04-19 19:15:08 -0500

    correccion

--- yhondanilo/chess.rb
+++ yhondanilo/chess.rb
@@ -8,21 +8,21 @@ class Chess
   end
 
   def intercalar
-    @board=Array.new(@fila){ Array.new(@columna) }
+    @board = Array.new(@fila){ Array.new(@columna) }
     @board[0][0] = 1
-    for f in 0..self.fila-1
-      for c in 0..self.columna-1
-        if @board[f][c-1]==0
+    for f in 0..self.fila - 1
+      for c in 0..self.columna - 1
+        if @board[f][c - 1] == 0
           @board[f][c] =  1
-        elsif @board[f][c-1]==1
-          @board[f][c]=0
+        elsif @board[f][c - 1] == 1
+          @board[f][c] = 0
         end
-        if c==@columna-1
-          ultimo=@board[f][c]
-          if ultimo==0
-            @board[f+1][c]=1
-          elsif ultimo==1
-            @board[f+1][c]=0
+        if c == @columna - 1
+          ultimo = @board[f][c]
+          if ultimo == 0
+            @board[f + 1][c] = 1
+          elsif ultimo == 1
+            @board[f + 1][c] = 0
           end
         end
       end
@@ -30,8 +30,8 @@ class Chess
   end
 
   def imprimir
-    for f in 0..self.fila-1
-      for c in 0..self.columna-1
+    for f in 0..self.fila - 1
+      for c in 0..self.columna - 1
         puts @board[f][c]
       end
     end
@@ -42,5 +42,5 @@ puts "Ingrese el numero de filas"
 filas = gets.to_i
 puts "Ingrese el numero de columnas"
 columnas = gets.to_i
-chess_board=Chess.new(filas, columnas)
+chess_board = Chess.new(filas, columnas)
 chess_board.imprimir
