1bfd13bec07ae502481a9bc88b53f55681b1e75f
Yhon Danilo Tabares Ramirez <yhondanilo@gmail.com>
2015-04-24 12:45:12 -0500

    ChessBoard2

--- yhondanilo/chess2.rb
+++ yhondanilo/chess2.rb
@@ -1,25 +1,19 @@
 class Chess
-
   attr_accessor :ultimo_color, :columnas, :filas
 
   def initialize(filas, columnas)
     @ultimo_color = 0
     @filas, @columnas = filas, columnas
-    #imprimir
   end
 
   def intercalar
-
     @board = Array.new(@filas){ Array.new(@columnas) }
     for f in 1..self.filas
       for c in 1..self.columnas
         @ultimo_color = get_color
-        @board[f][c] = @ultimo_color
-       
-      end
-      
+        @board[f][c] = @ultimo_color       
+      end      
     end
-
   end
 
   def get_color
@@ -28,14 +22,6 @@ class Chess
     when 1 then 0
     end
   end
-
-  #def imprimir
-  #  for f in 1..self.filas
-  #    for c in 1..self.columnas
-  #      puts @board[f][c]
-  #    end
-  #  end
-  #end
 end
 
 puts "Ingrese el numero de filas"
